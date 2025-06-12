91f5b950f638123e9a1a765dc470b3d4ac79d546
Ali Issa <alyy.issa01@gmail.com>
2025-06-12 18:32:09 +0300

    Added routes and cart/product-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { HomeComponent } from './home/home.component';
-
+import { ProductListComponent } from "./product-list/product-list.component";
+import { CartComponent } from "./cart/cart.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, HomeComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent, ProductListComponent, CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
